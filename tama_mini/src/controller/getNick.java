238e39a0b25392a3f5e7f1537d294f8900783ea7
smhrd <smhrd@59.0.129.219>
2022-01-19 17:55:38 +0900

    update getNick

--- tama_mini/src/controller/getNick.java
+++ tama_mini/src/controller/getNick.java
@@ -6,6 +6,40 @@ import model.tamagotchiDAO;
 import model.tamagotchiVO;
 
 public class getNick {
+	public void allNick() {
+        tamagotchiDAO a = new tamagotchiDAO();
+        ArrayList<tamagotchiVO> al = a.rankList();
+        ArrayList<tamagotchiVO> al2 = a.alivedTamaList();
+        System.out.println("----------------------------Don't overlap.-----------------------------");
+        System.out.println("=======================================================================");
+        int cnt1 = 0;
+        int cnt2 = 0;
+        if(al.size() != 0) {
+        System.out.println("==============================Nick(dead)===============================");
+        for(int i=0; i<al.size(); i++) {
+           System.out.printf("%s |", al.get(i).getNick());
+           cnt1++;
+           if(cnt1 % 8 == 0) {
+        	   System.out.println();        	  
+           }
+        }
+        System.out.println();
+        System.out.println("=======================================================================");
+        }
+        if(al2.size() != 0) {
+        System.out.println("==============================Nick(alive)==============================");
+        for(int i=0; i<al2.size(); i++) {
+           System.out.printf("%s |", al2.get(i).getNick());
+           cnt2++;
+           if(cnt2 % 8 == 0) {
+        	   System.out.println();        	  
+           }
+        }  System.out.println();
+           System.out.println("=======================================================================");
+        }
+        
+     }
+	
 	public String getUserNick(String user_id) {
 		   tamagotchiDAO a = new tamagotchiDAO();
 		   ArrayList<tamagotchiVO> al = null;
